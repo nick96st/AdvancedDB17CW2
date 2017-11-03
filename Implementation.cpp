62bf5a01e50015df67009bd9be04d29f5d756b53
Holger Pirk <hlgr@cloud-vm-45-211>
2017-11-03 12:26:28 +0000

    added some output to stubs

--- Implementation.cpp
+++ Implementation.cpp
@@ -1,16 +1,19 @@
 #include "Implementation.hpp"
+#include <iostream>
 
 //////////////////// Nested Loop Joins ////////////////////
 
 std::vector<std::string> getQualifyingBusinessesIDsVector(Businesses const& b, float latMin,
 																													float latMax, float longMin,
 																													float longMax) {
-	throw "unimplemented";
+	std::cout << "function getQualifyingBusinessesIDsVector not implemented" << endl;
+	throw std::logic_error("unimplemented");
 }
 
 std::vector<unsigned long> performNestedLoopJoinAndAggregation(Reviews const& r,
 																												std::vector<std::string> const& qualies) {
-	throw "unimplemented";
+	std::cout << "function performNestedLoopJoinAndAggregation not implemented" << endl;
+	throw std::logic_error("unimplemented");
 }
 
 //////////////////// Hash Join ////////////////////
@@ -18,11 +21,13 @@ std::vector<unsigned long> performNestedLoopJoinAndAggregation(Reviews const& r,
 std::unordered_set<std::string> getQualifyingBusinessesIDs(Businesses const& b, float latMin,
 																													 float latMax, float longMin,
 																													 float longMax) {
-	throw "unimplemented";
+	std::cout << "function getQualifyingBusinessesIDs not implemented" << endl;
+	throw std::logic_error("unimplemented");
 }
 
 std::vector<unsigned long>
 aggregateStarsOfQualifyingBusinesses(Reviews const& r,
 																		 std::unordered_set<std::string> const& qualifyingBusinesses) {
-	throw "unimplemented";
+		std::cout << "function aggregateStarsOfQualifyingBusinesses not implemented" << endl;
+	throw std::logic_error("unimplemented");
 }
