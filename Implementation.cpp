7dd0cb412eb4f800e48675dcdd20dd750dcc3771
Holger Pirk <hlgr@dyn1227-112.wlan.ic.ac.uk>
2017-11-03 13:51:36 +0000

    added brief description of the functions

--- Implementation.cpp
+++ Implementation.cpp
@@ -7,12 +7,21 @@ using namespace std;
 std::vector<std::string> getQualifyingBusinessesIDsVector(Businesses const& b, float latMin,
 																													float latMax, float longMin,
 																													float longMax) {
+	// This function needs to find all businesses that have within the
+	// specified latitude/longitude range and store their ids in the result vector
 	std::cout << "function getQualifyingBusinessesIDsVector not implemented" << std::endl;
 	throw std::logic_error("unimplemented");
 }
 
 std::vector<unsigned long>
-performNestedLoopJoinAndAggregation(Reviews const& r, std::vector<std::string> const& qualies) {
+performNestedLoopJoinAndAggregation(Reviews const& r, std::vector<std::string> const& qualifyingBusinessesIDs) {
+	// The second parameter of this function is the vector of qualifying
+	// business ids you have created in the first function
+
+	// This function needs to find all reviews that have business_ids in
+	// the qualifyingBusinessesIDs vector and build a histogram over their stars
+	// The return value is that histogram
+
 	std::cout << "function performNestedLoopJoinAndAggregation not implemented" << std::endl;
 	throw std::logic_error("unimplemented");
 }
@@ -22,6 +31,8 @@ performNestedLoopJoinAndAggregation(Reviews const& r, std::vector<std::string> c
 std::unordered_set<std::string> getQualifyingBusinessesIDs(Businesses const& b, float latMin,
 																													 float latMax, float longMin,
 																													 float longMax) {
+	// This function needs to find all businesses that have within the
+	// specified latitude/longitude range and store their ids in the result set
 	std::cout << "function getQualifyingBusinessesIDs not implemented" << std::endl;
 	throw std::logic_error("unimplemented");
 }
@@ -29,6 +40,12 @@ std::unordered_set<std::string> getQualifyingBusinessesIDs(Businesses const& b, 
 std::vector<unsigned long>
 aggregateStarsOfQualifyingBusinesses(Reviews const& r,
 																		 std::unordered_set<std::string> const& qualifyingBusinesses) {
+	// The second parameter of this function is the set of qualifying
+	// business ids you have created in the first function
+
+	// This function needs to find all reviews that have business_ids in
+	// the qualifyingBusinessesIDs vector and build a histogram over their stars
+	// The return value is that histogram
 	std::cout << "function aggregateStarsOfQualifyingBusinesses not implemented" << std::endl;
 	throw std::logic_error("unimplemented");
 }
